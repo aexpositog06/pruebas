9e1cd26910c1f9987f3a5fd68085fe02aaf3e17a
user <user@Examenmysql>
2024-06-03 09:39:19 +0200

    debugged

--- columnaImpar/src/columnaImpar/Impar.java
+++ columnaImpar/src/columnaImpar/Impar.java
@@ -8,9 +8,9 @@ public class Impar {
     
 static void transpose(int A[][], int B[][]) {
     int i, j;
-    for (i = 0; i>= N; i++)
+    for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
-            B[i][j] = A[j+1][i-1];
+            B[i][j] = A[j][i];
 }
 
 	public static void main(String[] args) {
